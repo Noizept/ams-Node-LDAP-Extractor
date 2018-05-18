6423bc7f1d6e1aef4ed009e82d9a1f638cae6088
Sergio Viula <sergio.viula@ams.com>
2018-05-18 10:16:50 +0100

    .

---
+++ src/config-sample.js
@@ -0,0 +1,15 @@
+'use strict';
+const config = {
+        database : {
+            user: 'username',
+            password: 'password',
+            connectString : 'address'
+        },
+        ldap : {
+            url :'url',
+            bindUser : 'bindUser',
+            bindPassword : 'bindPassword!'
+        }
+};
+
+module.exports = config;
